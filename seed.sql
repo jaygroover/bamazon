399c10eb81e6794ac94e13ce36b3fc93f586bbe7
jaygroover1 <jaygroover@gmail.com>
2019-05-16 00:16:21 -0400

    final update

--- seed.sql
+++ seed.sql
@@ -1,7 +1,15 @@
 INSERT INTO products(productname, departmentname,price,stockquantity)
-VALUES ("Milk","food",3.00,30),
-("eggs","food",3,30),
-("bannans","food",2,30),
-("pizza","food",5,30);
+VALUES ("galon milk","grocery",3.00,130),
+(" dozen eggs","grocery",3,150),
+("bannans","grocery",2,30),
+("pizza","grocery",5,30),
+("steak","grocery",8,45),
+("pork chops","grocery",6,30),
+("chicken breast","grocery",5,60),
+("tv","electronics",400,110),
+("laptop","electronics",900,80),
+("tablet","electronics",600,65),
+("monitors","electronics",250,210);
+
 
 SELECT * FROM bamazon.products;
